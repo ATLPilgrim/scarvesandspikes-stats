a76a16ff491d7a7491055b473a6b86bef6d6cc46
ATLPilgrim <typilgs@gmail.com>
2026-02-20 19:11:34 -0500

    Update opponent.js

--- api/opponent.js
+++ api/opponent.js
@@ -101,7 +101,7 @@ export default async function handler(req, res) {
           season: season,
           matchday: game.matchday,
           isHome,
-          isPlayoff: game.game_type !== 'Regular Season'
+          isPlayoff: game.game_type !== 'Regular Season',
           opponent: oppInfo.name,
           opponentAbbr: oppInfo.abbr,
           opponentId: oppId,
